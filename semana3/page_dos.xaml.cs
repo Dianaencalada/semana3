7ae9a8a9c92b498440ef0812a3a3bfb769d6f13d
master <master@SMLAB408PC18_22>
2022-04-18 21:02:50 -0500

    Agregar archivos de proyecto.

---
+++ semana3/page_dos.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace semana3
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class page_dos : ContentPage
+    {
+        public page_dos(string User,string Pass)
+        {
+            InitializeComponent();
+            //DisplayAlert("Mensaje", "El usuario es:" + User + "La contraseña es:" + Pass, "cerrar");
+            lblUser.Text = User;
+            lblPass.Text = Pass;
+            
+        }
+    }
+}
